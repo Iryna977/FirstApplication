3cc2e7d4e17d5daa50bc16b556b0566cecc1d837
Iryna977 <iokravets@gmail.com>
2021-02-26 18:29:45 +0200

    name

--- FirstApplication.ConsoleApp/Program.cs
+++ FirstApplication.ConsoleApp/Program.cs
@@ -7,9 +7,15 @@ namespace FirstApplication.ConsoleApp
     {
        static void Main(string[] args)
         {
-            string MyName;
-            MyName = "Iryna";
-            Console.WriteLine(MyName);
+            string MyName = "Iryna";
+            byte MyAge = 33;
+            bool HaveIApet = false;
+            double MyShoeSize = 36;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("My age is " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
             Console.ReadKey();
         }
     }
